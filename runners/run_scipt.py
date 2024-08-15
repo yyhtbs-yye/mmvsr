81bdb6e6afbe65b189a82223500718f8f48c5abe
yyhtbs-yye <yyhtbs1@gmail.com>
2024-08-15 20:19:35 +0100

    first commit

---
+++ runners/run_scipt.py
@@ -0,0 +1,28 @@
+
+import os
+
+cuda_id = 2
+cfg_path = "configs/n_to_n_vsr.py"
+
+model_configs = dict(type='BasicVSRPlusPlusNet', mid_channels=16, num_blocks=2,
+                     is_low_res_input=True,
+                     spynet_pretrained='https://download.openmmlab.com/mmediting/restorers/'
+                     'basicvsr/spynet_20210409-c6c1bd09.pth')
+
+os.environ['CUDA_VISIBLE_DEVICES'] = str(cuda_id)
+
+import torch  # Assuming PyTorch is the backend
+from mmengine.runner import Runner
+from mmengine.config import Config
+
+cfg = Config.fromfile(cfg_path)
+
+cfg.model['generator'].update(**model_configs)
+
+# Below will not work, as they are not modified in settings but as global variables. 
+cfg.train_dataloader['dataset']['num_input_frames'] = 7
+cfg.val_dataloader['dataset']['num_input_frames'] = 7
+cfg.work_dir = './work_dirs/BasicVSRPlusPlusNet'
+runner = Runner.from_cfg(cfg)
+
+runner.train()
